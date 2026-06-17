5ccbb8a46bc193c8b1d4bebaf0a801bbbba34b37
viraone <vxayananh@gmail.com>
2026-06-16 23:27:48 -0700

    Add Supabase database persistence

--- supabase-config.js
+++ supabase-config.js
@@ -1,6 +1,6 @@
 window.GRAVY_SUPABASE_CONFIG = {
   url: "https://sphfrtuahzwlqjnmwguq.supabase.co",
   // Use your public anon/publishable key here. Do not use an sb_secret key in GitHub Pages.
-  anonKey: "YOUR_SUPABASE_ANON_OR_PUBLISHABLE_KEY",
+  anonKey: "sb_publishable_gPHdHHAwHQ6K3c2iz5DZIg_9IreI0K2",
   profileId: "00000000-0000-0000-0000-000000000001",
 };
