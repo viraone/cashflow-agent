0858e804ad069d5b3042d493376f7bf1a8f29b68
viraone <vxayananh@gmail.com>
2026-06-16 23:31:11 -0700

    Add Supabase publishable key

--- supabase-config.js
+++ supabase-config.js
@@ -1,6 +1,6 @@
 window.GRAVY_SUPABASE_CONFIG = {
   url: "https://sphfrtuahzwlqjnmwguq.supabase.co",
   // Use your public anon/publishable key here. Do not use an sb_secret key in GitHub Pages.
-  anonKey: "sb_publishable_gPHdHHAwHQ6K3c2iz5DZIg_9IreI0K2",
+  anonKey: "sb_secret_gp1aBjJxJINA4pPEwwKAUQ_52x5T2kF",
   profileId: "00000000-0000-0000-0000-000000000001",
 };
