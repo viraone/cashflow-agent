36096fbc186c2faae3f74137dee4206552d791ab
viraone <vxayananh@gmail.com>
2026-06-17 00:16:35 -0700

    Update Supabase config

---
+++ dist-pages/supabase-config.js
@@ -0,0 +1,8 @@
+window.GRAVY_SUPABASE_CONFIG = {
+  
+  url: "https://sphfrtuahzwlqjnmwguq.supabase.co",
+  // Use your public anon/publishable key here. Do not use an sb_secret key in GitHub Pages.
+  anonKey: "sb_publishable_gPHdHHAwHQ6K3c2iz5DZIg_9IreI0K2",
+
+  profileId: "00000000-0000-0000-0000-000000000001",
+};
